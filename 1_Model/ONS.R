b2a87f5fde277c866bcf1496461014912317294e
Michelle S. Lee <chelles2@stanford.edu>
2015-12-05 11:48:09 +0000

    Split up ONS population projections by 65-74, 75-84, 85+

--- 1_Model/ONS.R
+++ 1_Model/ONS.R
@@ -3,46 +3,40 @@ library(reshape2)
 
 # Clean ONS Population data and aggregate up to 65+ and 85+ elderly populations
 
-setwd("~/DataKind/2015 DataDive Bristol/1. Raw Data")
+setwd("~/DataDive Workings/ONS Source")
 pop <- read.csv("ONS_Population_Forecast.csv", check.names = FALSE)
 head(pop)
 
 m <- melt(pop, id = c("ONS.LA.Code", "ONS.LA.Name", "Age.Group"), variable = "Year", value.name = "Population")
 head(m)
 
-age65above <- aggregate(Population ~ ONS.LA.Code + ONS.LA.Name + Year, FUN = sum, subset = Age.Group != "All ages", data = m)
-names(age65above)[4] <- "Pop.Age.65pl"
+age65.74 <- aggregate(Population ~ ONS.LA.Code + ONS.LA.Name + Year, FUN = sum, subset = c(Age.Group == "65-69" | Age.Group == "70-74"), data = m)
+names(age65.74)[4] <- "Pop.Age.65.74"
+
+
+age75.84 <- aggregate(Population ~ ONS.LA.Code + ONS.LA.Name + Year, FUN = sum, subset = c(Age.Group == "75-79" | Age.Group == "80-84"), data = m)
+names(age75.84)[4] <- "Pop.Age.75.84"
 
 age85above <- aggregate(Population ~ ONS.LA.Code + ONS.LA.Name + Year, FUN = sum, subset = c(Age.Group == "85-89" | Age.Group == "90+"), data = m)
 names(age85above)[4] <- "Pop.Age.85pl"
 head(age85above)
-pop65.85 <- merge(age65above, age85above, by = c("ONS.LA.Code", "ONS.LA.Name", "Year"))
+pop65.85 <- merge(age65.74, age75.84, by = c("ONS.LA.Code", "ONS.LA.Name", "Year"))
+pop65.85 <- merge(pop65.85, age85above, by = c("ONS.LA.Code", "ONS.LA.Name", "Year"))
+
 head(pop65.85)
 
 # Need to combine City of London and Westminster & Cornwall and Isles of Scilly
 aggs <- subset(pop65.85, ONS.LA.Code == "E09000033" | ONS.LA.Code == "E09000001")
 pop65.85 <- pop65.85[pop65.85$ONS.LA.Code != "E09000033" & pop65.85$ONS.LA.Code != "E09000001", ]
 
-d <- ddply(aggs, .(Year), summarise, ONS.LA.Code = "E09000033/E09000001", ONS.LA.Name = "Westminster,City of London", Pop.Age.65pl = sum(Pop.Age.65pl), Pop.Age.85pl = sum(Pop.Age.85pl))
+d <- ddply(aggs, .(Year), summarise, ONS.LA.Code = "E09000033/E09000001", ONS.LA.Name = "Westminster,City of London", Pop.Age.65.74 = sum(Pop.Age.65.74), Pop.Age.75.84 = sum(Pop.Age.75.84), Pop.Age.85pl = sum(Pop.Age.85pl))
 pop65.85 <- as.data.frame(rbind(pop65.85, d))
 
 aggs <- subset(pop65.85, ONS.LA.Code == "E06000052" | ONS.LA.Code == "E06000053")
 pop65.85 <- pop65.85[pop65.85$ONS.LA.Code != "E06000052" & pop65.85$ONS.LA.Code != "E06000053", ]
-d <- ddply(aggs, .(Year), summarise, ONS.LA.Code = "E06000052/E06000053", ONS.LA.Name = "Cornwall,Isles of Scilly", Pop.Age.65pl = sum(Pop.Age.65pl), Pop.Age.85pl = sum(Pop.Age.85pl))
+d <- ddply(aggs, .(Year), summarise, ONS.LA.Code = "E06000052/E06000053", ONS.LA.Name = "Cornwall,Isles of Scilly", Pop.Age.65.74 = sum(Pop.Age.65.74), Pop.Age.75.84 = sum(Pop.Age.75.84), Pop.Age.85pl = sum(Pop.Age.85pl))
 pop65.85 <- as.data.frame(rbind(pop65.85, d))
 unique(pop65.85$ONS.LA.Name)
 
-# Resident Care Occupants
-res <- read.csv("ONS_ResCareOccupants2011.csv")
-head(res)
-
-res$Year <- "2011"
-head(pop65.85)
-
-pop65.85$Pop.Age.65pl <- pop65.85$Pop.Age.65pl*1000
-pop65.85$Pop.Age.85pl <- pop65.85$Pop.Age.85pl*1000
-
-df <- merge(pop65.85, res, by = c("ONS.LA.Code", "ONS.LA.Name", "Year"), all.x = TRUE) #from 9836 to 9490 because of exclusion of Wales
-head(df) 
-
-write.csv(df, "ONS_Cleaned.csv", row.names = FALSE)
+setwd("~/DataKind/1_Model")
+write.csv(df, "SRC - CLEAN - ONS Population Projections by Age.csv", row.names = FALSE)
