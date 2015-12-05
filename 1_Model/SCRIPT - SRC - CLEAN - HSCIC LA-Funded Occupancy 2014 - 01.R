1c9581979981e7d3254a8f69af6059680fb70c2a
Michelle S. Lee <chelles2@stanford.edu>
2015-12-05 15:31:15 +0000

    NEW HSCIC data from "S2" and updated ONS with 1,000 figures (unit conversion)

--- 1_Model/SCRIPT - SRC - CLEAN - HSCIC LA-Funded Occupancy 2014 - 01.R
+++ 1_Model/SCRIPT - SRC - CLEAN - HSCIC LA-Funded Occupancy 2014 - 01.R
@@ -1,13 +1,17 @@
 library(plyr)
 library(reshape2)
 
-setwd("~/DataKind/1_Model")
-hscic <- read.csv("HSCIC_2014_Pop.csv", check.names = FALSE)
+setwd("~/DataDive Workings/HSCIC Source")
+hscic <- read.csv("S2_Final.csv", check.names = FALSE)
+head(hscic)
+hscic$Total <- NULL
 
-m <- melt(hscic, id = c("ItemBaseID", "TextName", "Primary_Category", "Age_Category", "CompSerTypeCode", "TypeStayCode"), variable = "HSCIC_Region", value.name = "N_Council_Funded_Occupancy")
+m <- melt(hscic, id = c("AgeCategory", "ServiceType"), variable = "HSCIC_Region", value.name = "N_Council_Funded_Occupancy")
 head(m)
 
+setwd("~/DataDive Workings/1. Raw Data")
 LA.map <- read.csv("HSCIC_ONS_LA_Mapping.csv")
+head(LA.map)
 
 hscic.mapped <-merge(m, LA.map, by = "HSCIC_Region", all.x = TRUE)
 head(hscic.mapped)
@@ -20,26 +24,16 @@ aggs <- subset(hscic.mapped, ONS.LA.Code == "E09000033" | ONS.LA.Code == "E09000
 aggs
 hscic.mapped <- hscic.mapped[hscic.mapped$ONS.LA.Code != "E09000033" & hscic.mapped$ONS.LA.Code != "E09000001", ]
 
-d <- ddply(aggs, .(TextName, Primary_Category, Age_Category, CompSerTypeCode, TypeStayCode), summarise, ONS.LA.Code = "E09000033/E09000001", ONS.LA.Name = "Westminster,City of London", ItemBaseID = "Combined", N_Council_Funded_Occupancy = sum(N_Council_Funded_Occupancy))
+head(hscic.mapped)
+d <- ddply(aggs, .(AgeCategory, ServiceType), summarise, ONS.LA.Code = "E09000033/E09000001", ONS.LA.Name = "Westminster,City of London", N_Council_Funded_Occupancy = sum(N_Council_Funded_Occupancy))
 hscic.mapped <- as.data.frame(rbind(hscic.mapped, d))
 
 aggs <- subset(hscic.mapped, ONS.LA.Code == "E06000052" | ONS.LA.Code == "E06000053")
 hscic.mapped <- hscic.mapped[hscic.mapped$ONS.LA.Code != "E06000052" & hscic.mapped$ONS.LA.Code != "E06000053", ]
-d <- ddply(aggs, .(TextName, Primary_Category, Age_Category, CompSerTypeCode, TypeStayCode), summarise, ONS.LA.Code = "E06000052/E06000053", ONS.LA.Name = "Cornwall,Isles of Scilly", ItemBaseID = "Combined", N_Council_Funded_Occupancy = sum(N_Council_Funded_Occupancy))
+d <- ddply(aggs, .(AgeCategory, ServiceType), summarise, ONS.LA.Code = "E06000052/E06000053", ONS.LA.Name = "Cornwall,Isles of Scilly", N_Council_Funded_Occupancy = sum(N_Council_Funded_Occupancy))
 hscic.mapped <- as.data.frame(rbind(hscic.mapped, d))
 unique(hscic.mapped$ONS.LA.Name)
 
+head(hscic.mapped)
+
 write.csv(hscic.mapped, "HSCIC_2014_Occupancy_Mapped.csv")
-
-hscic.by.age <- ddply(hscic.mapped, .(Age_Category, ONS.LA.Code, ONS.LA.Name), summarise, N_Council_Funded_Occupancy = sum(N_Council_Funded_Occupancy))
-head(hscic.by.age)
-write.csv(hscic.by.age, "HSCIC_2014_Occupancy_Aggregate_By_Age_And_LA.csv")
-
-# Put res and nursing in different columns
-df <- read.csv("SRC - CLEAN - HSCIC LA-Funded Occupancy 2014.csv")
-head(df)
-hscic.by.age <- ddply(df, .(Age_Category, ONS.LA.Code, ONS.LA.Name, CompSerTypeCode), summarise, N_Council_Funded_Occupancy = sum(N_Council_Funded_Occupancy))
-head(hscic.by.age)
-m <- melt(hscic.by.age)
-head(m)
-head(dcast(m, Age_Category + ONS.LA.Code + ONS.LA.Name + variable ~ CompSerTypeCode))
